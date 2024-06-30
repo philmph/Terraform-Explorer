d0ef6ab48e396becd622199f384c641a19f84e30
Philipp Maier <25420070+philmph@users.noreply.github.com>
2024-06-30 12:40:34 +0200

    Add loop-defaults

---
+++ loop-defauls/README.md
@@ -0,0 +1,37 @@
+# loop-defaults
+
+I wanted to test if using `_` actually has any use in Terraform as compared to go.
+
+To test this locally, run `terraform init` and run `terraform console` after.
+
+## Notes
+
+Using `_` does not make a lot of sense imho as it is still usable. The only valid reason i see is opinionated in maps and objects:
+
+If you want to use only the key instead of the value you could write f.e.
+
+```hcl
+locals {
+obj = {
+  key1 = "value1"
+}
+
+obj_loop = [ for key, _ in local.obj : ... ]
+}
+```
+
+This would not actually dump the value but indicate to a human that it is not relevant / used in the loop.
+
+Using only `key` would result in `key` being the `value`:
+
+```hcl
+locals {
+obj = {
+  key1 = "value1"
+}
+
+obj_loop = [ for key in local.obj : "I am actually the value=${key}" ]
+}
+```
+
+Not of any use in lists imho because it will result in a iterators `0, 1, ...`.
---
+++ loop-defauls/main.tf
@@ -0,0 +1,50 @@
+locals {
+  object = {
+    key1 = "value1"
+    key2 = "value2"
+  }
+
+  # Full typed loop
+  object_loop_fully_typed = [for key, value in local.object : "key=${key}, value=${value}"]
+
+  # Providing only one variable will default to the value
+  object_loop_omit = [for value in local.object : "value=${value}"]
+
+  # Using _ does not /dev/null like in go, _ is still usable
+  object_loop_using_ = [for _, value in local.object : "key=${_}, value=${value}"]
+
+  # Opinionated valid use case: Only wanting the key but indicating (to humans) that the value is not used
+  object_loop_key_only = [for key, _ in local.object : "key=${key}, I am still usable=${_}"]
+
+  # Using key only results in key being the value as also seen in the object_loop_omit
+  object_loop_key_wrongfully_expected = [for key in local.object : "I am actually the value=${key}"]
+
+  map = tomap({
+    key1 = "value1"
+    key2 = "value2"
+  })
+
+  tuple = [
+    "value1",
+    "value2",
+  ]
+
+  # Full typed loop
+  tuple_loop_fully_typed = [for iterator, obj in local.tuple : "iterator=${iterator}, obj=${obj}"]
+
+  # Providing only one variable will default to the object
+  tuple_loop_omit = [for obj in local.tuple : "obj=${obj}"]
+
+  # Using _ does not /dev/null like in go, _ is still usable
+  tuple_loop_using_ = [for _, obj in local.tuple : "iterator=${_}, obj=${obj}"]
+
+  list = tolist([
+    "value1",
+    "value2",
+  ])
+
+  set = toset([
+    "value1",
+    "value2",
+  ])
+}
