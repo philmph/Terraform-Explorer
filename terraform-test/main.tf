9b7fd2675add22870ff2d25d9df21293fe1e45b0
Philipp Maier <25420070+philmph@users.noreply.github.com>
2024-08-11 09:54:43 +0200

    Add resource_group tests

--- terraform-test/main.tf
+++ terraform-test/main.tf
@@ -25,18 +25,16 @@ variable "name" {
 }
 
 # Add random characters to the storage account name to ensure it is unique
-resource "random_string" "this" {
-  count = 0
+# resource "random_string" "this" {
+#   length = 24 - 2 - length(var.name)
 
-  length = 24 - 2 - length(var.name)
-
-  special = false
-  upper   = false
-}
+#   special = false
+#   upper   = false
+# }
 
 locals {
-  resource_group_name  = "rg-${var.name}"
-  storage_account_name = "sa${var.name}${random_string.this.result}"
+  resource_group_name = "rg-${var.name}"
+  # storage_account_name = "sa${var.name}${random_string.this.result}"
 }
 
 resource "azurerm_resource_group" "this" {
@@ -44,14 +42,12 @@ resource "azurerm_resource_group" "this" {
   location = "westeurope"
 }
 
-resource "azurerm_storage_account" "this" {
-  count = 0
+# resource "azurerm_storage_account" "this" {
+#   name                = local.storage_account_name
+#   resource_group_name = azurerm_resource_group.this.name
+#   location            = azurerm_resource_group.this.location
 
-  name                = local.storage_account_name
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-
-  account_kind             = "StorageV2"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+#   account_kind             = "StorageV2"
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
