cf5e4c8337e36cb671fc58b440eb2291ba5418d7
Philipp Maier <25420070+philmph@users.noreply.github.com>
2024-08-11 09:47:29 +0200

    Enable only Resource Group

--- terraform-test/main.tf
+++ terraform-test/main.tf
@@ -26,6 +26,8 @@ variable "name" {
 
 # Add random characters to the storage account name to ensure it is unique
 resource "random_string" "this" {
+  count = 0
+
   length = 24 - 2 - length(var.name)
 
   special = false
@@ -33,19 +35,22 @@ resource "random_string" "this" {
 }
 
 locals {
+  resource_group_name  = "rg-${var.name}"
   storage_account_name = "sa${var.name}${random_string.this.result}"
 }
 
 resource "azurerm_resource_group" "this" {
-  name     = "rg-${var.name}"
+  name     = local.resource_group_name
   location = "westeurope"
 }
 
 resource "azurerm_storage_account" "this" {
+  count = 0
+
+  name                = local.storage_account_name
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
 
-  name                     = "storageaccountname"
   account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
