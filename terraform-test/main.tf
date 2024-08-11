d8da1c1ee9e21fdcaa65fff0f01a341cc990fcf6
Philipp Maier <25420070+philmph@users.noreply.github.com>
2024-08-11 14:34:16 +0200

    Add (not very useful) showcase tests

--- terraform-test/main.tf
+++ terraform-test/main.tf
@@ -25,16 +25,17 @@ variable "name" {
 }
 
 # Add random characters to the storage account name to ensure it is unique
-# resource "random_string" "this" {
-#   length = 24 - 2 - length(var.name)
+# 24 = Maximum length, 2 = "st" prefix, length(var.name) = length of the name
+resource "random_string" "this" {
+  length = 24 - 2 - length(var.name)
 
-#   special = false
-#   upper   = false
-# }
+  special = false
+  upper   = false
+}
 
 locals {
-  resource_group_name = "rg-${var.name}"
-  # storage_account_name = "sa${var.name}${random_string.this.result}"
+  resource_group_name  = "rg-${var.name}"
+  storage_account_name = "st${var.name}${random_string.this.result}"
 }
 
 resource "azurerm_resource_group" "this" {
@@ -42,12 +43,12 @@ resource "azurerm_resource_group" "this" {
   location = "westeurope"
 }
 
-# resource "azurerm_storage_account" "this" {
-#   name                = local.storage_account_name
-#   resource_group_name = azurerm_resource_group.this.name
-#   location            = azurerm_resource_group.this.location
+resource "azurerm_storage_account" "this" {
+  name                = local.storage_account_name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
 
-#   account_kind             = "StorageV2"
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
