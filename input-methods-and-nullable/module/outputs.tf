dfe8bdd28cb4ecbff8b9692c7fdf6079f0635d5d
Philipp Maier <25420070+philmph@users.noreply.github.com>
2025-03-21 17:26:17 +0100

    Minor

--- input-methods-and-nullable/module/outputs.tf
+++ input-methods-and-nullable/module/outputs.tf
@@ -1,9 +1,9 @@
 output "nullable" {
-  description = "Test output for a"
+  description = "Test output for nullable"
   value       = var.nullable
 }
 
 output "not_nullable" {
-  description = "Test output for b"
+  description = "Test output for not_nullable"
   value       = var.not_nullable
 }
